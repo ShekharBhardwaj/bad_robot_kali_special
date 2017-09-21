f473f6d35d6fc4d8a2acea9407e768fa2df54aaf
ShekharBhardwaj <shekhar.nik@gmail.com>
2017-09-21 18:33:27 -0400

    Adding 5ghz scan support

--- scan_net.py
+++ scan_net.py
@@ -3,11 +3,18 @@ import os
 
 def scan():
 	try:
+		userchoice = input('''
+		1 for 2.4Ghz(Default)
+		2 for 5Ghz :''')
 		print('CAUTION: This script is for educational purposes only')
 		print('starting monitoring on wlan0')
 		os.system('airmon-ng start wlan0')
 		print('If you are not seeing any output make sure your wifi adapter is in place and working')
-		os.system('airodump-ng wlan0mon')
+		if userchoice == '1':
+			os.system('airodump-ng wlan0mon')
+		else:
+			os.system('airodump-ng wlan0mon --band a')
+
 	except ValueError as e:
 		print('Script failed due to ----> {}'.format(e))
 	finally:
